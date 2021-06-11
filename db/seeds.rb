d1cf95d9430a85ab8c6bbe6adf2f55ce80a0825a
Gregoire Bazetoux <gregoire.bazetoux@edhec.com>
2021-06-11 11:56:03 +0200

    seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -17,7 +17,7 @@ user_serialized = URI.open(url).read
 movies = JSON.parse(user_serialized)
 results = movies["results"]
 results.each do |movie|
-    Movie.create(title: movie["original_title"], overview: movie["overview"], poster_url: "https://image.tmdb.org/t/p/original#{movie["poster_path"]}", rating: movie["vote_average"])
+    Movie.create(title: movie["title"], overview: movie["overview"], poster_url: "https://image.tmdb.org/t/p/original#{movie["poster_path"]}", rating: movie["vote_average"])
 end
 
 
